e4b5977fdda2610742f085691aa0f28d9c72f305
Mahrukh Ali <118937416+mahrukhali@users.noreply.github.com>
2025-02-08 14:27:15 -0500

    Files

---
+++ client/src/pages/Columbia.js
@@ -0,0 +1,10 @@
+function Columbia() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Columbia University!</h1>
+      <p>Here you can find information related to Columbia.</p>
+    </div>
+  );
+}
+
+export default Columbia;
---
+++ client/src/pages/QueensCollege.js
@@ -0,0 +1,10 @@
+function QueensCollege() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to Queens College!</h1>
+      <p>Here you can find information related to Queens College.</p>
+    </div>
+  );
+}
+
+export default QueensCollege;
